242cc686331673e696c1c8efcabe65d17c154c6b
posmoda <posmoda@yahoo.co.jp>
2021-02-12 16:21:35 +0900

    0212

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import mariadb
+import re
 from flask import *
 from flask_cors import CORS
 
@@ -22,6 +23,20 @@ patients_list = [{
         "followingAblation": False
         }]
 
+def up_matched(m):
+    s = m.group(1)
+    return s.upper()
+
+def snake_to_camel(string):
+    pattern = re.compile(r'_([a-z])')
+    result = pattern.sub(up_matched, string)
+    return result
+
+def camel_to_snake(string):
+    pattern = re.compile(r'[A-Z]')
+    result = pattern.sub(r'_\0', string)
+    return result.lower()
+
 app = Flask(__name__)
 CORS(app)
 
@@ -32,6 +47,19 @@ def hello_world():
 @app.route('/api/patients', methods=['GET', 'POST'])
 def list_patients():
     return jsonify(patients_list)
+
+@app.route('/api/baseline/<int:patient_serial_number>', methods=['GET'])
+def give_baseline_data(patient_serial_number):
+    data = {
+            "patientSerialNumber": patient_serial_number,
+            "sex": 0
+            }
+    return jsonify(data)
+
+@app.route('/api/baseline/<int:patient_serial_number>', methods=['POST'])
+def update_baseline_data(patient_serial_number):
+    print(request.json)
+    return "ok"
 
 if __name__ == '__main__':
     app.run()
