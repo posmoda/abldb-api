d4c198e3ca95627a3c80f7d184efa72927909ac5
posmoda <posmoda@yahoo.co.jp>
2021-02-24 08:39:22 +0900

    0223

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import mariadb
 import re
+from datetime import date, datetime
 from flask import *
 from flask_cors import CORS
 
@@ -23,8 +24,8 @@ def snake_to_camel(string):
     return result
 
 def camel_to_snake(string):
-    pattern = re.compile(r'[A-Z]')
-    result = pattern.sub(r'_\0', string)
+    pattern = re.compile(r'([A-Z])')
+    result = pattern.sub(r'_\1', string)
     return result.lower()
 
 def format_dic_keys(tuple_of_dic, direction):
@@ -51,6 +52,40 @@ def validiate_token(token):
         return 0
     else:
         return None
+
+def format_value(value):
+    if value is None:
+        return "NULL"
+    elif isinstance( value, int ):
+        return str( value )
+    elif isinstance( value, date ):
+        return "\'" + value.strptime( "%Y-%m-%d" ) + "\'"
+    elif value == True:
+        return "\'b\'1"
+    elif  value == False:
+        return"\'b\'0"
+    else:
+        return f"\'{str( value )}\'"
+
+def format_data_to_insert(data):
+    data = format_dic(data, 'snake')
+    data_pairs = map( lambda k: k + " = " + format_value( data[k] ), data )
+    return ','.join( data_pairs )
+
+def format_data_to_cast(data):
+    data  = format_dic( data, 'camel' )
+    print( data )
+    data_to_cast = {}
+    for k, v in data.items():
+        if v == b'\x00':
+            data_to_cast[k] = False
+        elif v == b'\x01':
+            data_to_cast[k] = True
+        elif isinstance( v, date ):
+            data_to_cast[k] = v.strftime( '%Y-%m-%d' )
+        else:
+            data_to_cast[k] = v
+    return data_to_cast
 
 app = Flask(__name__)
 CORS(app)
@@ -98,13 +133,23 @@ def give_baseline_data(patient_serial_number):
     '''
     cursor.execute( query )
     baseline = cursor.fetchone()
-    baseline['sex'] = int.from_bytes( baseline['sex'], byteorder='big' )
     cursor.close()
-    return jsonify( format_dic( baseline, 'camel' ))
+    print( format_data_to_cast( baseline ) )
+    return jsonify( format_data_to_cast( baseline ))
 
 @app.route('/api/baseline/<int:patient_serial_number>', methods=['POST'])
 def update_baseline_data(patient_serial_number):
-    print(request.json)
+    baseline_data = format_data_to_insert( request.json )
+    query = f'''
+        UPDATE `patients` 
+            SET { baseline_data }
+        WHERE `patient_serial_number` = {patient_serial_number};
+    '''
+    print( query )
+    cursor = get_cursor()
+    cursor.execute( query )
+    connection.commit()
+    cursor.close()
     return "ok"
 
 if __name__ == '__main__':
